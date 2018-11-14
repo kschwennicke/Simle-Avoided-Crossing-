b7276cafa6f5ef0c3599dabf6bc63ad9b9b17ce1
kschwennicke <45022113+kschwennicke@users.noreply.github.com>
2018-11-13 18:03:21 -0800

    Update RK4.py

--- RK4.py
+++ RK4.py
@@ -1,15 +1,23 @@
 import numpy as np
 
 
-def C_dot_k(C,x,v,k,V,d):
+def C_dot_k(C,x,v,k,V,d):                       # differential equaion for Cdotk. C is an array of inital coefficients
+                                                # x,v are the posion and velocity of the particle
+                                                # k specifies what Cdot you want
+                                                # V is the undiagonlized potential matrix
+                                                # d i the coupling matrix 
     eigvals = np.linalg.eigvalsh(V(x))
-    V = np.matrix([[eigvals[0],0],[0,eigvals[1]]])
+    V = np.matrix([[eigvals[0],0],[0,eigvals[1]]]) #diagonilized V matrix at posion x 
     a0 = C[0]*(V[k,0]-complex(0,v *d(x)[k,0]))
     a1=C[1]*(V[k,1]-complex(0,v *d(x)[k,1]))
     a = (a0+a1)*complex(0,-1)
     return a
 
-def Ck (C,xi,x1_2,xf,vi,v1_2,vf,k,dt,V,d):
+def Ck (C,xi,x1_2,xf,vi,v1_2,vf,k,dt,V,d):              # employ RK4 to find new coefficiet Ck
+                                                        #C -> array of inicial coefficients 
+                                                        # xi, x1_2,xf are the initial posion, potion half way through the time step, and the final postion
+                                                        # vi, v1_2,vf are the velocities """""""""""""
+                                                        # k labels the coeficient you want, dt is the time step, V is the undiagolized matrix, d is the coupling matrix 
         C0 = C[0]
         C1 =C[1]
         if k ==0:
